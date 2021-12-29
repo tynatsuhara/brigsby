96997bfc3426524f3bc9b207fd3617f82a28e6f1
Tyler Bonnell <tbonnell@uw.edu>
2021-12-29 13:06:38 -0800

    add execution index to repeatedinvoker callback

--- src/util/RepeatedInvoker.ts
+++ src/util/RepeatedInvoker.ts
@@ -6,14 +6,15 @@ import { UpdateData } from "../Engine"
  * The invoked function runs in the update() step.
  */
 export class RepeatedInvoker extends Component {
-    private fn: (updateData: UpdateData) => number
+    private fn: (updateData: UpdateData, execution: number) => number
     private delay: number
+    private execution = 0
 
     /**
      * @param fn The function to execute. Returns the delay (in milliseconds) until the next invocation.
      * @param initialDelay The delay before the function executes for the first time.
      */
-    constructor(fn: (updateData: UpdateData) => number, initialDelay = 0) {
+    constructor(fn: (updateData: UpdateData, execution: number) => number, initialDelay = 0) {
         super()
         this.fn = fn
         this.delay = initialDelay
@@ -22,7 +23,8 @@ export class RepeatedInvoker extends Component {
     update(updateData: UpdateData) {
         this.delay -= updateData.elapsedTimeMillis
         if (this.delay < 0) {
-            this.delay = this.fn(updateData)
+            this.delay = this.fn(updateData, this.execution)
+            this.execution++
         }
     }
 }
